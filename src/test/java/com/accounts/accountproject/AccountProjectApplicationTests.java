2b655cf6bb09e37ab2a27c2cc1237f8650ec5dfa
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-09-13 21:54:33 +0200

    Fix Spring Boot context test

--- src/test/java/com/accounts/accountproject/AccountProjectApplicationTests.java
+++ src/test/java/com/accounts/accountproject/AccountProjectApplicationTests.java
@@ -2,8 +2,12 @@ package com.accounts.accountproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+//Can be passed only with existing interfaces implementation
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AccountProjectApplicationTests {
 
     @Test
